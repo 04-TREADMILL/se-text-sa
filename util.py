7c7b7a3406feaf5f8f04436cdef10062a0d93375
lingyuchen <201250007@smail.nju.edu.cn>
2023-05-26 01:27:57 +0800

    feat: gen fine-tune dataset from csv train dataset

---
+++ util.py
@@ -0,0 +1,15 @@
+import csv
+
+
+if __name__ == '__main__':
+    with open('dataset/fine_tune_set.jsonl', encoding='utf-8', mode='w') as f1:
+        with open('dataset/train3098itemPOLARITY.csv', encoding='utf-8', mode='r') as f2:
+            file_reader = csv.reader(f2, delimiter=';')
+            dataset = [(line[2], line[1]) for line in file_reader]
+            for text, label in dataset:
+                text = text.replace('\\', '\\\\')
+                text = text.replace('"', '\\"')
+                line = '{"prompt": "' + text + ' ->", "completion": "' + label + '"}\n'
+                f1.write(line)
+        f2.close()
+    f1.close()
