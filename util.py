0c21460d9e0fdf10a8426360daf27b2472b3f033
lingyuchen <201250007@smail.nju.edu.cn>
2023-05-31 11:21:24 +0800

    ...

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 import csv
+import random
 
 
-if __name__ == '__main__':
+def gen_fine_tune_dataset():
     with open('dataset/fine_tune_set.jsonl', encoding='utf-8', mode='w') as f1:
         with open('dataset/train3098itemPOLARITY.csv', encoding='utf-8', mode='r') as f2:
             file_reader = csv.reader(f2, delimiter=';')
@@ -9,7 +10,30 @@ if __name__ == '__main__':
             for text, label in dataset:
                 text = text.replace('\\', '\\\\')
                 text = text.replace('"', '\\"')
-                line = '{"prompt": "' + text + ' ->", "completion": "' + label + '"}\n'
+                line = f'{{"prompt": "{text} ->", "completion": " {label}"}}\n'
                 f1.write(line)
         f2.close()
     f1.close()
+
+
+def split_dataset():
+    with open('dataset/fine_tune_set.jsonl', encoding='utf-8', mode='r') as f:
+        random.seed(7)
+        train_set = []
+        validate_set = []
+        for line in f.readlines():
+            if random.random() < 0.2:
+                validate_set.append(line)
+            else:
+                train_set.append(line)
+        with open('./dataset/fine_tune_train_set.jsonl', encoding='utf-8', mode='w') as f1, \
+                open('./dataset/fine_tune_validate_set.jsonl', encoding='utf-8', mode='w') as f2:
+            f1.writelines(train_set)
+            f2.writelines(validate_set)
+        f1.close()
+        f2.close()
+    f.close()
+
+
+if __name__ == '__main__':
+    gen_fine_tune_dataset()
