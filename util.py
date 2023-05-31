addcdddef7fdfb5099baf70dfeb393f69155f4e1
lingyuchen <201250007@smail.nju.edu.cn>
2023-05-31 23:25:51 +0800

    feat: add function for data preparation

--- util.py
+++ util.py
@@ -1,10 +1,19 @@
 import csv
 import random
 
+from textattack.augmentation import EmbeddingAugmenter
 
-def gen_fine_tune_dataset():
-    with open('dataset/fine_tune_set.jsonl', encoding='utf-8', mode='w') as f1:
-        with open('dataset/train3098itemPOLARITY.csv', encoding='utf-8', mode='r') as f2:
+from analyzer import Analyzer
+from preProcessing import PreProcessor
+
+
+def gen_fine_tune_dataset(fine_tune_suffix: str = '', base_suffix: str = ''):
+    fine_tune_set_name = f'dataset/fine_tune_set_{fine_tune_suffix}.jsonl'\
+        if fine_tune_suffix != '' else 'dataset/fine_tune_set.jsonl'
+    base_set_name = f'dataset/train3098itemPOLARITY_{base_suffix}.csv'\
+        if base_suffix != '' else 'dataset/train3098itemPOLARITY.csv'
+    with open(fine_tune_set_name, encoding='utf-8', mode='w') as f1:
+        with open(base_set_name, encoding='utf-8', mode='r') as f2:
             file_reader = csv.reader(f2, delimiter=';')
             dataset = [(line[2], line[1]) for line in file_reader]
             for text, label in dataset:
@@ -14,6 +23,17 @@ def gen_fine_tune_dataset():
                 f1.write(line)
         f2.close()
     f1.close()
+
+
+def gen_preprocessed_dataset(origin_file_name: str):
+    pp = PreProcessor()
+    texts, labels = Analyzer.read_data(origin_file_name)
+    texts = [pp.preprocess_text_v2(text) for text in texts]
+    with open('./dataset/train3098itemPOLARITY_preprocessed.csv', mode='w', encoding='utf-8') as file:
+        for text, label in zip(texts, labels):
+            line = f'xx;{label};{text}\n'
+            file.write(line)
+    file.close()
 
 
 def split_dataset():
@@ -35,5 +55,28 @@ def split_dataset():
     f.close()
 
 
+def augment_dataset():
+    texts_train, labels_train = Analyzer.read_data('dataset/train3098itemPOLARITY.csv')
+    texts_train_augmented = []
+    labels_train_augmented = []
+    augmenter = EmbeddingAugmenter(transformations_per_example=2)
+    index = 0
+    print('start augment')
+    for text, label in zip(texts_train, labels_train):
+        text_aug_res = augmenter.augment(text)
+        label_aug_res = [label] * len(text_aug_res)
+        texts_train_augmented.extend(text_aug_res)
+        labels_train_augmented.extend(label_aug_res)
+        print(f'finish augment text {index}')
+        index += 1
+    print('finish augment')
+    with open('dataset/train3098itemPOLARITY_augmented.csv', mode='w', encoding='utf-8') as f:
+        for text, label in zip(texts_train_augmented, labels_train_augmented):
+            line = f'xx;{label};{text}\n'
+            f.write(line)
+    f.close()
+
+
 if __name__ == '__main__':
-    gen_fine_tune_dataset()
+    gen_preprocessed_dataset('dataset/train3098itemPOLARITY.csv')
+    gen_fine_tune_dataset(fine_tune_suffix='preprocessed', base_suffix='preprocessed')
